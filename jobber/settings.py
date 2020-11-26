8492f43488cbf9f12b56e8f49bfec1bd01b77e2e
JamesLee <tekmickey0118@gmail.com>
2020-11-26 00:07:33 +0000

    fixed static

--- jobber/settings.py
+++ jobber/settings.py
@@ -142,7 +142,15 @@ SWAGGER_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = { 'default': dj_database_url.config() }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
