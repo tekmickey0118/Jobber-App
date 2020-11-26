0edc7c343b60f9f0f76d4e24c2bd65969492f803
JamesLee <tekmickey0118@gmail.com>
2020-11-26 17:49:50 +0000

    db changed

--- jobber/settings.py
+++ jobber/settings.py
@@ -143,14 +143,9 @@ SWAGGER_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': 'jobber',
-        'USER': 'jobber',
-        'PASSWORD': 'jobber123',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'CONN_MAX_AGE': 60
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
