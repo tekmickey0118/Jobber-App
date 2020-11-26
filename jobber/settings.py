d32d32f50806c2982ba4cd71e9ff04aa1c0b15bb
JamesLee <tekmickey0118@gmail.com>
2020-11-26 00:03:19 +0000

    fixed static

--- jobber/settings.py
+++ jobber/settings.py
@@ -212,8 +212,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
