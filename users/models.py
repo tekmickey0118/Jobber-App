29a90e7b59185b424f7f80060937df1129f298c4
JamesLee <tekmickey0118@gmail.com>
2020-11-27 00:01:09 +0000

    fixed gender choices

--- users/models.py
+++ users/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import UserManager
-
+import urllib.request
+import mimetypes
 
 class User(AbstractUser):
+
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+    GENDER_NON_BINARY = 'N'
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male', ), 
+        (GENDER_FEMALE, 'Female', ), 
+        (GENDER_NON_BINARY, 'Non Binary', ),
+    )
+
     username = models.CharField(max_length=255, null=False)
     profile_pic = models.ImageField(upload_to = '', blank = True)
     email = models.EmailField(max_length=255, unique=True)
     reg_number = models.TextField(max_length = 9, unique = True)
     hostel_room = models.TextField()
     phone = models.CharField(max_length = 12)
-    gender = models.TextField()
+    gender = models.TextField(choices=GENDER_CHOICES)
     total_tasks = models.IntegerField(default = 0)
     completed_tasks = models.IntegerField(default = 0)
     uncompleted_tasks = models.IntegerField(default = 0)
@@ -22,6 +33,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    def save_image(user):
+        url = username
+        filename = user.id + mimettypes.guess_extension(urllib.request.urlopen(url).headers['content-type'])
+        path = os.path.join(settings.MEDIA_ROOT, filename)
+
+        urllib.request.urlretrieve(url, path)
+
+        user.image = path
+        user.save()
 
 #to put tasks integer field to this model----------
 class User_status(models.Model):
