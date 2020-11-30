6e1313133ab1e7b7c4150c91718d7dd98a80ddeb
JamesLee <tekmickey0118@gmail.com>
2020-11-30 12:43:23 +0000

    changed username field

--- jobberwork/views.py
+++ jobberwork/views.py
@@ -191,7 +191,7 @@ class ItemPickedStatusView(APIView):
         picked_object = ItemPicked.objects.get(task = tasks)
         print(assigned)
         if picked.exists():
-            return Response({'message':'your item has been picked up from the destination...','task':tasks.Title, 'task_location': tasks.location,'time_picked': picked_object.Time, 'delivery_user': assigned.username,
+            return Response({'message':'your item has been picked up from the destination...','task':tasks.Title, 'task_location': tasks.location,'time_picked': picked_object.Time, 'delivery_user': assigned.first_name,
             'delivery_user_phone': assigned.phone}) #maybe something else like on your way...
         else:
             return Response({'message':'Item has not been picked'})
