2c76efa5b32d66d785c0127e7420a84f3b661008
JamesLee <tekmickey0118@gmail.com>
2020-12-15 17:24:18 +0530

    online access

--- jobber/settings.py
+++ jobber/settings.py
@@ -157,7 +157,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'offline',
+            'access_type': 'online',
         },
     },
 }
